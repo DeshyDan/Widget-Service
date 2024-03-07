32cdcff25efc97d7a18cc69fab2f2fc595319a3a
Deshy Dan <deshy733@gmail.com>
2024-03-07 09:44:28 +0200

    Export Widget service to Github

---
+++ lib/my_widget_service-stack.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as widget_service from '../lib/widget_service'
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class MyWidgetServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new widget_service.WidgetService(this, 'Widgets');
+  }
+}
